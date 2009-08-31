33294948a3c71d763d1fc77315a63d7290a8d3e6
Will Gray <graywh@gmail.com>
2009-08-31 12:19:04 -0500

    That looks better.

--- 1995/4/2-4.rb
+++ 1995/4/2-4.rb
@@ -2,14 +2,14 @@
 
 chk = [1,0,0,0,0,0,1]
 File.readlines('prob4.in').each do |line|
-  line = line.strip().chars.to_a.collect { |c| c.to_i }
+  line = line.strip.chars.collect { |c| c.to_i }
   recording = false
   line.each_slice(7) do |slice|
     if slice == chk
       recording = !recording
     elsif recording
       sum = 0
-      (0..6).each { |j| sum += slice[j] * (2 ** (6 - j)) }
+      slice.each_with_index { |a,j| sum += a * (2 ** (6 - j)) }
       print sum.chr
     end
   end
