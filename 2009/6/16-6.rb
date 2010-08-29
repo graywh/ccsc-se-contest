915adc3168fbb4c6e73994be3bc73178147f0fc9
Will Gray <graywh@gmail.com>
2010-08-28 22:32:11 -0500

    Another 2009 solutions in Ruby.

---
+++ 2009/6/16-6.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+def count(n, m, so_far=[])
+  l = so_far.length
+  if l == n
+    c = 1
+  else
+    if l == 0
+      a = 1
+    else
+      a = so_far[-1] * 2
+    end
+    b = (m / 2**(n-l-1)).to_i
+    if b < a
+      c = 0
+    else
+      c = (a..b).collect do |x|
+        count(n, m, so_far + [x])
+      end.inject(&:+)
+    end
+  end
+  return c
+end
+
+STDIN.readline
+STDIN.readlines.each_with_index do |line, i|
+  n, m = line.split.collect(&:to_i)
+  number = count(n, m)
+  puts "Data set #{i+1}: #{n} #{m} #{number}"
+end
